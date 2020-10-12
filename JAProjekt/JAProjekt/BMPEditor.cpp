58943846991375d96498269847a77df0e9db1e28
michalpatryk <43810475+michalpatryk@users.noreply.github.com>
2020-10-12 14:54:27 +0200

    runAlgorithm main skeleton added

--- JAProjekt/JAProjekt/BMPEditor.cpp
+++ JAProjekt/JAProjekt/BMPEditor.cpp
@@ -60,29 +60,92 @@ std::string BMPEditor::runAlgorithm(AlgorithmType algType, unsigned int threadCo
 	{
 		return headerParserError.value();
 	}
-
+	//TODO
+	//Add rewriting everything between ios::begin and bfOffBits
+	
 	//Memory check	
 	getMemoryStatus();
 	DWORDLONG memsize = statex.ullTotalPhys;
 	DWORDLONG maxProgramMemUse = memsize * 2 / 3;	//maximum 70% memory load
 
-	//Thread vectorization
-	std::vector<std::thread> threadVector;
+	maxProgramMemUse = maxProgramMemUse - (maxProgramMemUse % threadCount);
+	DWORD remainingFileSize = fileHeader.bfSize;
 
+	//Main alg loop
+	while (maxProgramMemUse < remainingFileSize)
+	{
+		char* arrToSplit = new char[maxProgramMemUse];
+		fileStream.read(arrToSplit, maxProgramMemUse);
+		
+		DWORDLONG splitValue = maxProgramMemUse / threadCount;
+
+		std::vector<std::thread> threadVector;
+		for (unsigned int i = 0; i < threadCount; i++)
+		{
+			for ( unsigned int j = 0 ; j*splitValue < maxProgramMemUse; j++)
+			{
+				if (algType == AlgorithmType::cppAlgorithm)
+				{
+					//std::thread t(RUNCPPALG(arrToSplit + j, arrToSplit + j+ splitValue));
+				}
+				//else { std::thread t(RUNASMALG(arrToSplit + j, arrToSplit + j+ splitValue)); };
+			}
+			
+		}
+		//outStream.write(arrToSplit, maxProgramMemUse);
+		delete[] arrToSplit;
+		remainingFileSize -= maxProgramMemUse;
+	}
+
+	char* arrToSplit = new char[remainingFileSize];
+	fileStream.read(arrToSplit, remainingFileSize);
+	DWORDLONG extra = remainingFileSize % threadCount;
+
+	DWORDLONG splitValue = remainingFileSize / threadCount;
+
+	std::vector<std::thread> threadVector;
+	for (unsigned int i = 0; i < threadCount; i++)
+	{
+		for (unsigned int j = 0; j * splitValue < maxProgramMemUse; j++)
+		{
+			int k = j * splitValue;
+			if (algType == AlgorithmType::cppAlgorithm)
+			{
+				if (k > maxProgramMemUse)
+				{
+					//std::thread t(RUNCPPALG(arrToSplit + j, arrToSplit + j + splitValue + extra));
+				}
+				//std::thread t(RUNCPPALG(arrToSplit + j, arrToSplit + j + splitValue));
+				
+			}
+			else
+			{
+				if (k > maxProgramMemUse)
+				{
+					//std::thread t(RUNASMALG(arrToSplit + j, arrToSplit + j + splitValue + extra));
+				}
+				//std::thread t(RUNASMALG(arrToSplit + j, arrToSplit + j + splitValue + extra));
+			}
+		}
+
+	}
+	//outStream.write(arrToSplit, maxProgramMemUse + extra);
+	delete[] arrToSplit;
+	
 	//TODO
-	//Add full copy of anything between ios::begin and fileHeader.bfOffBits to output file
-	//Add disk avaliable space test/warning
-	//	--QueryPerformanceCounter-- START
-	//All bellow in while ifstream+chunkSize > ios::end
-	//	Add chunk read
-	//	Add chunk division
-	//	Add divided chunk process (by dll)
-	//	Add processed chunk save
-	//Add last chunk read
-	//Add last divided chunk process (by dll)
-	//Add last chunk save
-	//	--QueryPerformanceCounter-- END
-	//	Add extra timer - without file reads/writes
+	//Add full copy of anything between ios::begin and fileHeader.bfOffBits to output file									X
+	//Add disk avaliable space test/warning																					X
+	//	--QueryPerformanceCounter-- START																					X
+	//All bellow in while ifstream+chunkSize > ios::end																		X
+	//	Add chunk read																										+
+	//	Add chunk division																									+
+	//	Add divided chunk process (by dll)																					X
+	//	Add processed chunk save																							X
+	//Add last chunk read																									+
+	//Add last divided chunk process (by dll)																				+
+	//Add last chunk save																									X
+	//	--QueryPerformanceCounter-- END																						X
+	//	Add extra timer - without file reads/writes																			X
 	
 	fileStream.close();
 	return std::string();
